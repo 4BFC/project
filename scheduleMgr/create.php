2259098f86e375627d68792db6e9d84355863ca1
4BFC <4bee.code@gmail.com>
2023-11-12 22:56:30 +0900

    new create scheduleMgr dir

---
+++ scheduleMgr/create.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Schedule</title>
+</head>
+
+<body>
+  <h1>Create</h1>
+  <form action="process_create.php">
+    <p>title</p>
+    <!--  -->
+    <p><input type="text" name="title"></p>
+    <p>description</p>
+    <!--  -->
+    <p><textarea name="description" id="" cols="30" rows="10"></textarea></p>
+    <input type="submit" value="create">
+  </form>
+  <a href="index.php">Home</a>
+  <a href="">NULL</a>
+</body>
+
+</html>
---
+++ scheduleMgr/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Schedule</title>
+</head>
+
+<body>
+  <h1>Schedule</h1>
+  <h2>My Schedule</h2>
+  <a href="create.php">Make Schedule</a>
+  <a href="">NULL</a>
+</body>
+
+</html>
